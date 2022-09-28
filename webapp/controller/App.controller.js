83ab1d63847712296d59c7f56c6d250c816588ea
Abir18 <rifatabir21@gmail.com>
2022-09-28 17:45:51 +0600

    Upto Step 7

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/ui/model/json/JSONModel"
+    ],
+    function (Controller, MessageToast, JSONModel) {
+        "use strict";
+        return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+            onInit: function () {
+                let oData = {
+                    recipient: {
+                        name: "World"
+                    }
+                };
+                let oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+            },
+            onShowHello: function () {
+                // show a native JavaScript alert
+                MessageToast.show("Hello World");
+            }
+        });
+    }
+);
