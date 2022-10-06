95085fdcfd685fef84dec75897d1c9c05106135d
Abir18 <rifatabir21@gmail.com>
2022-10-06 19:18:26 +0600

    Upto Step 9

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,29 +1,8 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast",
-        "sap/ui/model/json/JSONModel",
-        "sap/ui/model/resource/ResourceModel"
-    ],
-    function (Controller, MessageToast, JSONModel, ResourceModel) {
+    ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+    function (Controller, MessageToast) {
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-            onInit: function () {
-                // set data model on view
-                const oData = {
-                    recipient: {
-                        name: "Finally UI5"
-                    }
-                };
-                const oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
-
-                // set i18n model on view
-                const i18nModel = new ResourceModel({
-                    bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-                });
-                this.getView().setModel(i18nModel, "i18n");
-            },
             onShowHello: function () {
                 const oBundle = this.getView()
                     .getModel("i18n")
@@ -31,7 +10,8 @@ sap.ui.define(
                 const sRecipient = this.getView()
                     .getModel()
                     .getProperty("/recipient/name");
-                const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+                const sMsg = oBundle.getText("helloMsg", [sRecipient, 33]);
                 // show message
                 MessageToast.show(sMsg);
             }
