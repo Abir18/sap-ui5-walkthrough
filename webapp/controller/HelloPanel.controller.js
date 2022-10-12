6d21c9ae15d1b49885909de2a09335776b467c76
Abir18 <rifatabir21@gmail.com>
2022-10-12 19:26:58 +0600

    Upto Step 22

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,16 +1,46 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-    "use strict";
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/ui/core/Fragment"
+    ],
+    function (Controller, MessageToast, Fragment) {
+        "use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-        onShowHello: function () {
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView()
-                .getModel()
-                .getProperty("/recipient/name");
+        return Controller.extend(
+            "sap.ui.demo.walkthrough.controller.HelloPanel",
+            {
+                onShowHello: function () {
+                    const oBundle = this.getView()
+                        .getModel("i18n")
+                        .getResourceBundle();
+                    const sRecipient = this.getView()
+                        .getModel()
+                        .getProperty("/recipient/name");
 
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // show message
-            MessageToast.show(sMsg);
-        }
-    });
-});
+                    const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                    // show message
+                    MessageToast.show(sMsg);
+                },
+
+                onOpenDialog: function () {
+                    // create dialog lazily
+                    if (!this.pDialog) {
+                        this.pDialog = this.loadFragment({
+                            name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                        });
+                    }
+                    this.pDialog.then(function (oDialog) {
+                        oDialog.open();
+                    });
+                },
+
+                onCloseDialog: function () {
+                    // note: We don't need to chain to the pDialog promise, since this event-handler
+                    // is only called from within the loaded dialog itself.
+                    this.byId("helloDialog").close();
+                }
+            }
+        );
+    }
+);
