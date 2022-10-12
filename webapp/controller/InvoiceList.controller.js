66682abae4c1f7675dbcafc07975e2c5a6c2ce9f
Abir18 <rifatabir21@gmail.com>
2022-10-12 19:54:41 +0600

    Upto Step 23

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -2,9 +2,11 @@ sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
         "sap/ui/model/json/JSONModel",
-        "../model/formatter"
+        "../model/formatter",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator"
     ],
-    function (Controller, JSONModel, formatter) {
+    function (Controller, JSONModel, formatter, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend(
@@ -16,6 +18,25 @@ sap.ui.define(
                         currency: "EUR"
                     });
                     this.getView().setModel(oViewModel, "view");
+                },
+                onFilterInvoices: function (oEvent) {
+                    // build the filter array
+                    let aFilter = [];
+                    let sQuery = oEvent.getParameter("query");
+                    if (sQuery) {
+                        aFilter.push(
+                            new Filter(
+                                "ProductName",
+                                FilterOperator.Contains,
+                                sQuery
+                            )
+                        );
+                    }
+
+                    // filter binding
+                    let oList = this.byId("invoiceList");
+                    let oBinding = oList.getBinding("items");
+                    oBinding.filter(aFilter);
                 }
             }
         );
